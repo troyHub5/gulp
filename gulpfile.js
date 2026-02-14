46e8c8fe2fee3dbf36ef627cf451f7833435038a
troyHub5 <myworkchel@outlook.com>
2026-02-14 19:23:53 +0300

    myGulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const del = require('del');
+const fileInclude = require('gulp-file-include');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const cleanDist = require('gulp-clean');
 
 
 const paths = {
@@ -21,17 +24,21 @@ const paths = {
 };
 
 function clean() {
-  return del(['dist']);
-}   
+  return gulp.src('./dist/', { read:false, allowempty: true})
+    .pipe(cleanDist());
+}
+
 
 function styles() {
     return gulp.src(paths.styles.src)
+        .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(cleanCSS())
         .pipe(rename({
           basename: 'main',
           suffix: '.min'
         }))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(paths.styles.dest));
 }
 
@@ -44,16 +51,26 @@ function scripts() {
     .pipe(concat('main.min.js'))
 }
 
+function includeHTML() {
+  return gulp.src('src/**/*.html')
+    .pipe(fileInclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(gulp.dest('dist/'));
+}
+
 function watch(){
   gulp.watch(paths.styles.src, styles);
   gulp.watch(paths.scripts.src, scripts);
 }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts), watch); 
+const build = gulp.series(clean, gulp.series(styles, scripts), watch); 
 
-exports.clean = clean
+exports.clean = clean;
 exports.styles = styles;
 exports.scripts = scripts;
 exports.watch = watch;
 exports.build = build;
 exports.default = build;
+exports.includeHTML = includeHTML;
